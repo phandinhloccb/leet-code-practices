7dc6d745a08dc499838fdd322ceaca3470d2b9f0
l-phan <l-phan@creationline.com>
2025-06-14 14:36:28 +0900

    add 151

---
+++ 151.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func reverseWords(s string) string {
+	words := strings.Fields(s)
+
+	fmt.Printf("Type of", words)
+
+	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+		words[i], words[j] = words[j], words[i]
+	}
+
+	return strings.Join(words, " ")
+}
+
+func main() {
+	fmt.Println(reverseWords("the sky is blue"))
+}
+
+
+
+
+/*
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+*/
