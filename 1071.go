b48f7d6a31625d9ca17d294d6e9fc59429412805
l-phan <l-phan@creationline.com>
2025-06-28 10:10:11 +0900

    add 1732

--- 1071.go
+++ 1071.go
@@ -1,3 +1,5 @@
+package main
+
 func gcdOfStrings(str1 string, str2 string) string {
 	if str1+str2 != str2+str1 {
 		return ""
@@ -5,7 +7,7 @@ func gcdOfStrings(str1 string, str2 string) string {
 
 	gcdLength := gcd(len(str1), len(str2))
 	return str1[:gcdLength]
-}	
+}
 
 func gcd(a, b int) int {
 	for b != 0 {
