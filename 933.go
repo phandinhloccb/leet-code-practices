08cc8cf8c82f90c54c8e1dacbf3b2025aedf0dbb
l-phan <l-phan@creationline.com>
2025-07-12 13:45:49 +0900

    add 933

---
+++ 933.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+
+type RecentCounter struct {
+	request []int
+}
+
+
+func Constructor() RecentCounter {
+    return RecentCounter{
+        requests: []int{},
+    }
+}
+
+
+func (this *RecentCounter) Ping(t int) int {
+    this.requests = append(this.requests, t)
+
+	for len(this.requests) > 0 && this.requests[0] < t - 3000 {
+		this.requests = this.requests[1:]
+	}
+
+	return len(this.requests)
+}
+
+func main(){
+	rc := Constructor()
+	fmt.Println(rc.Ping(1))
+	fmt.Println(rc.Ping(100))
+	fmt.Println(rc.Ping(3001))
+	fmt.Println(rc.Ping(3002))
+}
